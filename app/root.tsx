f18319f4b4fd1814f35250ff97388e95f5c8c0bf
istiakbinmahmod <istiak.mahmod@optimizely.com>
2025-06-18 09:08:41 +0600

    add script

--- app/root.tsx
+++ app/root.tsx
@@ -54,6 +54,7 @@ export default function App() {
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="#0ea5e9" />
+        <script src="https://cdn.optimizely.com/js/6371880462974976.js"></script>
         <Meta />
         <Links />
       </head>
